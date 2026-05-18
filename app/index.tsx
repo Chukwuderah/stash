30e48bd86bc2e06fd4c0883fc13d5e72483d364a
Chukwuderah <pleasantchukwu@gmail.com>
2026-05-18 23:25:36 +0100

    feat: updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
+import colors from "@/constants/colors";
+import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { ImageBackground, Text, TouchableOpacity } from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { LinearGradient } from "expo-linear-gradient";
-import colors from "@/constants/colors";
 
 export default function Index() {
   return (
@@ -12,7 +12,10 @@ export default function Index() {
       resizeMode="cover"
       className="flex-1"
     >
-      <LinearGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]} className="flex-1">
+      <LinearGradient
+        colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        className="flex-1"
+      >
         <SafeAreaView className="p-6 flex flex-col justify-end flex-1 gap-3">
           <Animated.View
             entering={FadeInDown.delay(300)
@@ -20,8 +23,13 @@ export default function Index() {
               .stiffness(80)
               .springify(20)}
           >
-            <Text className="text-4xl text-white font-extrabold">Welcome to Stash</Text>
-            <Text className="text-2xl font-medium mt-2" style={{ color: colors.textOnDark }}>
+            <Text className="text-4xl text-white font-extrabold">
+              Welcome to Stash
+            </Text>
+            <Text
+              className="text-2xl font-medium mt-2"
+              style={{ color: colors.textOnDark }}
+            >
               Your personal idea parking lot.
             </Text>
           </Animated.View>
@@ -34,7 +42,7 @@ export default function Index() {
           >
             <TouchableOpacity
               activeOpacity={0.7}
-              onPress={() => router.push("/(tabs)")}
+              onPress={() => router.push("/(auth)/sign-up")}
               className="mt-4 bg-white w-full rounded-xl min-h-[62px] flex items-center justify-center"
             >
               <Text className="text-2xl font-semibold">Get Started</Text>
